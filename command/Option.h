59808a9379828e9910cd5c9a8d069b8b3b54290a
lfkdsk <lfk_dsk@hotmail.com>
2016-11-12 23:28:23 +0800

    add option.h

---
+++ command/Option.h
@@ -0,0 +1,8 @@
+//
+// Created by 刘丰恺 on 2016/11/12.
+//
+
+#ifndef JUSTVM_OPTION_H
+#define JUSTVM_OPTION_H
+
+#endif //JUSTVM_OPTION_H
---
+++ command/argument.h
@@ -0,0 +1,8 @@
+//
+// Created by 刘丰恺 on 2016/11/12.
+//
+
+#ifndef JUSTVM_ARGUMENT_H
+#define JUSTVM_ARGUMENT_H
+
+#endif //JUSTVM_ARGUMENT_H
