6399c459bd14e65b4a16005cdee95a1e28b256b7
lfkdsk <lfk_dsk@hotmail.com>
2016-11-12 22:42:58 +0800

    add behaviout.h optioninfo.h

---
+++ command/behaviour.h
@@ -0,0 +1,8 @@
+//
+// Created by 刘丰恺 on 2016/11/12.
+//
+
+#ifndef JUSTVM_BEHAVIOUR_H
+#define JUSTVM_BEHAVIOUR_H
+
+#endif //JUSTVM_BEHAVIOUR_H
---
+++ command/optionInfo.h
@@ -0,0 +1,8 @@
+//
+// Created by 刘丰恺 on 2016/11/12.
+//
+
+#ifndef JUSTVM_OPTIONINFO_H
+#define JUSTVM_OPTIONINFO_H
+
+#endif //JUSTVM_OPTIONINFO_H
