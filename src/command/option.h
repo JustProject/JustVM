47b177684be1ad89587f210d21732aa26ace4803
lfkdsk <lfk_dsk@hotmail.com>
2016-11-14 10:41:42 +0800

    update command needed

--- src/command/option.h
+++ src/command/option.h
@@ -11,80 +11,9 @@
 
 namespace cmd {
 
-    template<typename FunctionParamsType>
-    class Option : public Params, public Behaviour<FunctionParamsType> {
-    public:
-        Option(const std::string &name,
-               const std::string &optionInfo,
-               const std::function<void(FunctionParamsType)> &function)
-                : Behaviour<FunctionParamsType>(function), Params(optionInfo), name(name) { }
-
-
-        virtual ~Option() { }
-
-        virtual void handler() override {
-            this->call();
-            this->used = true;
-        }
-
-        /**
-         * Check argument for option
-         */
-        virtual bool check(const std::string &args) override {
-            if (this->contains(args)) {
-                std::size_t pos = this->valuePosition(args);
-
-                // lost value
-                if (pos != name.size()) {
-                    throw NeedOptionValue("Option " + name + " requires value !!!");
-                }
-
-                std::stringstream ss;
-                ss << std::fixed << args.substr(pos + 1);
-                ss >> value;
-
-                if (ss.fail()) {
-                    throw GetValueFail("Option: " + name + " get value failed !!!");
-                }
-
-                return true;
-            }
-
-            return false;
-        }
-
-        virtual unsigned int valuePosition(const std::string &string) override {
-            std::size_t pos = value.find("=");
-
-            if ((this->contains(value))
-                && (pos == std::string::npos)) {
-                throw OptionNotSpecified("Option: " + name + " with no value after equal sign !!!");
-            }
-
-            return (unsigned int) pos;
-        }
-
-
-    protected:
-        typedef std::string OptionName;
-        /**
-         * Current option value
-         */
-        FunctionParamsType value;
-
-        /*
-        * Current option name
-        */
-        const OptionName name;
-
-        bool contains(const std::string &argv) {
-            return argv.find(name) == 0;
-        }
-    };
-
     /**
-     * Option need value
-     */
+      * Option need value
+      */
     class NeedOptionValue : public std::invalid_argument {
 
     public:
@@ -111,6 +40,78 @@ namespace cmd {
 
         explicit OptionNotSpecified(const char *args)
                 : invalid_argument(args) { }
+    };
+
+
+    template<typename FunctionParamsType>
+    class Option : public Params, public Behaviour<FunctionParamsType> {
+    public:
+        Option(const std::string &name,
+               const std::string &optionInfo,
+               const std::function<void(FunctionParamsType)> &function)
+                : Behaviour<FunctionParamsType>(function), Params(optionInfo), name(name) { }
+
+
+        virtual ~Option() { }
+
+        virtual void handler() override {
+            this->call(value);
+            this->used = true;
+        }
+
+        /**
+         * Check argument for option
+         */
+        virtual bool check(const std::string &args) override {
+            if (this->contains(args)) {
+                int pos = this->valuePosition(args);
+
+                // lost value
+                if (pos != name.size()) {
+                    throw NeedOptionValue("Option " + name + " requires value !!!");
+                }
+
+                std::stringstream ss;
+                ss << std::fixed << args.substr(pos + 1);
+                ss >> value;
+
+                if (ss.fail()) {
+                    throw GetValueFail("Option: " + name + " get value failed !!!");
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        virtual unsigned int valuePosition(const std::string &value) override {
+            int pos = value.find("=");
+
+            if ((this->contains(value))
+                && (pos == std::string::npos)) {
+                throw OptionNotSpecified("Option: " + name + " with no value after equal sign !!!");
+            }
+
+            return (unsigned int) pos;
+        }
+
+
+    protected:
+        typedef std::string OptionName;
+        /**
+         * Current option value
+         */
+        FunctionParamsType value;
+
+        /*
+        * Current option name
+        */
+        const OptionName name;
+
+        bool contains(const std::string &argv) {
+            return argv.find(name) == 0;
+        }
     };
 
     template<>
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <multi_value.h>
 #include "logger/log.h"
 #include "command.h"
 
@@ -9,18 +10,40 @@ public:
     void help() {
         LOG(INFO) << "fuck helper -- ";
     };
+
+    void version() {
+        LOG(INFO) << "fuck version -- ";
+    }
+
+    void classPath(std::string cp) {
+        LOG(INFO) << "fuck cp -- " << cp;
+    }
 };
 
 int main(int argc, char *argv[]) {
 
     FuckDemo demo;
 
-    printf("%s", getenv("JAVA_HOME"));
+    printf("%s \n", getenv("JAVA_HOME"));
 
+    // print help message
     auto help =
             std::make_shared<cmd::Option<void>>("-h", "Help",
                                                 std::bind(&FuckDemo::help, &demo));
-    cmd::Command command(argc, argv, {help});
+    // vm version
+    auto version =
+            std::make_shared<cmd::Option<void>>("-v", "Version",
+                                                std::bind(&FuckDemo::version, &demo));
+    // multi parts of class path
+    auto multiPath =
+            std::make_shared<cmd::Option<std::string>>("-cp", "ClassPath",
+                                                       std::bind(&FuckDemo::classPath, &demo,
+                                                                 std::placeholders::_1));
+    auto classPath =
+            std::make_shared<cmd::MultiValue>(";", multiPath);
+
+
+    cmd::Command command(argc, argv, {help, version, classPath});
 
     return 0;
 }
