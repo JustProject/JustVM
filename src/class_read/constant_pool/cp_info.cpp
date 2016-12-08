282a48357b87aef29fedb2a984cce8a2d31e6cea
lfkdsk <lfk_dsk@hotmail.com>
2016-12-08 23:49:27 +0800

    add cp_info method

--- src/class_read/constant_pool/cp_info.cpp
+++ src/class_read/constant_pool/cp_info.cpp
@@ -26,3 +26,63 @@ cp_item *read_cp_item_from_bytes(bytes_reader &reader, BC_U1 u16) {
 
     return (cp_item *) item_struct;
 }
+
+void read_cp_meta_info(CONSTANT_Meta_info &, bytes_reader &reader) {
+
+}
+
+void read_cp_class_info(CONSTANT_Class_info &, bytes_reader &reader) {
+
+}
+
+void read_cp_field_ref_info(CONSTANT_Fieldref_info &, bytes_reader &reader) {
+
+}
+
+void read_cp_method_ref_info(CONSTANT_Methodref_info &, bytes_reader &reader) {
+
+}
+
+void read_cp_interface_method_ref_info(CONSTANT_InterfaceMethodref_info &, bytes_reader &reader) {
+
+}
+
+void read_cp_string_info(CONSTANT_String_info &, bytes_reader &reader) {
+
+}
+
+void read_cp_integer_info(CONSTANT_Integer_info &, bytes_reader &reader) {
+
+}
+
+void read_cp_float_info(CONSTANT_Float_info &, bytes_reader &reader) {
+
+}
+
+void read_cp_long_info(CONSTANT_Long_info &, bytes_reader &reader) {
+
+}
+
+void read_cp_double_info(CONSTANT_Double_info &, bytes_reader &reader) {
+
+}
+
+void read_cp_name_type_info(CONSTANT_NameAndType_info &, bytes_reader &reader) {
+
+}
+
+void read_cp_utf8_info(CONSTANT_Utf8_info &, bytes_reader &reader) {
+
+}
+
+void read_cp_method_handle_info(CONSTANT_MethodHandle_info &, bytes_reader &reader) {
+
+}
+
+void read_cp_method_type_info(CONSTANT_MethodType_info &, bytes_reader &reader) {
+
+}
+
+void read_cp_invoke_dynamic_info(CONSTANT_InvokeDynamic_info &, bytes_reader &reader) {
+
+}
