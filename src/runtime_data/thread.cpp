80e370fabf68a1c9709e4d036d5c5a9ac2ba2008
lfkdsk <lfk_dsk@hotmail.com>
2016-12-09 21:53:00 +0800

    delete comment in cp_info.cpp add thread in runtime_data

---
+++ src/runtime_data/thread.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 刘丰恺 on 2016/12/9.
+//
+
+#include "thread.h"
---
+++ src/runtime_data/thread.h
@@ -0,0 +1,14 @@
+//
+// Created by 刘丰恺 on 2016/12/9.
+//
+
+#ifndef JUSTVM_THREAD_H
+#define JUSTVM_THREAD_H
+
+
+class thread {
+
+};
+
+
+#endif //JUSTVM_THREAD_H
